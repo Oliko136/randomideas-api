d9f6e3ccd54815e3de40a54ac186f78bc78f47c5
Oliko136 <olchick136@gmail.com>
2025-09-28 13:37:11 +0400

    Create router for get requests

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const ideasRouter = require('./routes/ideas');
 const PORT = 8080;
 
 const app = express();
 
 app.get('/', (req, res) => {
-    res.json({ message: 'Hello World' });
+    res.json({ message: 'Welcome to the RandomIdeas API' });
 });
+
+app.use('/api/ideas', ideasRouter);
 
 // Explicitly bind to all interfaces
 app.listen(PORT, '0.0.0.0', () => {
