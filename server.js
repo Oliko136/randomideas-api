fb83250a26b63472afac8467566115b1a622003f
Oliko136 <olchick136@gmail.com>
2025-09-29 12:53:21 +0400

    Set up client folder

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 require('dotenv').config();
 
@@ -9,6 +10,9 @@ const connectDB = require('./config/db');
 connectDB();
 
 const app = express();
+
+// Static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
